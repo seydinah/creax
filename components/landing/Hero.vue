694fca9e6f632a1791b2cec65d511d348ab08c2f
seydinah <seydinah94@gmail.com>
2024-05-20 20:04:33 +0000

    updates

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -3,7 +3,7 @@
     <div class="p-24 md:order-1 hidden md:block">
       <img
         class="rounded-full transition-transform transform hover:scale-125"
-        src="~/assets/img/hero2.jpg"
+        src="~/assets/img/hero3.webp"
         alt="Starship starts the engine"
         loading="eager"
         format="avif"
